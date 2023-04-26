07ede61a9567bc9a7c5a054fc5d0bc4694111f9c
Siddharth Bhattacharjee <s34bhatt@uwaterloo.ca>
2023-04-26 01:09:31 -0400

    style: nit

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use rustc_hash::FxHasher;
 
 pub struct Cuckoo {
     buckets: Vec<Bucket>, // m buckets containing b entries
-    m: i32, 
+    num_buckets: i32, 
     max_kicks: i32,
 }
 
@@ -18,7 +18,7 @@ impl Cuckoo {
         max_kicks: i32,
     ) -> Self {
         Self {
-            m: num_buckets, 
+            num_buckets: num_buckets, 
             buckets: vec![Bucket::new(entries_per_bucket); num_buckets as usize],
             max_kicks: max_kicks,
         }
@@ -27,20 +27,17 @@ impl Cuckoo {
     pub fn insert(&mut self, x: &[u8]) -> bool {
         let mut rng = rand::thread_rng();
         let f = hash(x) as u8; 
-        let i1 = hash(x).rem_euclid(self.m);
-        let i2 = (i1 ^ hash(&[f])).rem_euclid(self.m);
-        println!("i1 {} {}", i1, i1 as usize); 
-        println!("i2 {} {}", i2, i2 as usize); 
+        let i1 = hash(x).rem_euclid(self.num_buckets);
+        let i2 = (i1 ^ hash(&[f])).rem_euclid(self.num_buckets);
 
         if self.buckets[i1 as usize].insert(f) || self.buckets[i2 as usize].insert(f) {
             return true;
         }
 
-        println!("could not insert, need to vacate");
         let mut i = [i1, i2][rng.gen_range(0..2)];
         for _ in 0..self.max_kicks {
             let e = self.buckets[i as usize].swap(f);
-            i = (i ^ hash(&[e])).rem_euclid(self.m);
+            i = (i ^ hash(&[e])).rem_euclid(self.num_buckets);
             println!("i {} {}", i, i as usize); 
             if self.buckets[i as usize].insert(e) {
                 return true;
@@ -52,16 +49,16 @@ impl Cuckoo {
 
     pub fn exists(&self, x: &[u8]) -> bool {
         let f = hash(x) as u8;
-        let i1 = hash(x).rem_euclid(self.m);
-        let i2 = (i1 ^ hash(&[f])).rem_euclid(self.m);
+        let i1 = hash(x).rem_euclid(self.num_buckets);
+        let i2 = (i1 ^ hash(&[f])).rem_euclid(self.num_buckets);
 
         return self.buckets[i1 as usize].lookup(f) || self.buckets[i2 as usize].lookup(f);
     }
 
     pub fn delete(&mut self, x: &[u8]) -> bool {
         let f = hash(x) as u8;
-        let i1 = hash(x).rem_euclid(self.m);
-        let i2 = (i1 ^ hash(&[f])).rem_euclid(self.m);
+        let i1 = hash(x).rem_euclid(self.num_buckets);
+        let i2 = (i1 ^ hash(&[f])).rem_euclid(self.num_buckets);
         
         if self.buckets[i1 as usize].lookup(f) {
             return self.buckets[i1 as usize].delete(f);
